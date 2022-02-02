57655f47b2b47da1d6084e95a12bc399dcd9bc7b
hisham8989 <khanhisham25@gmail.com>
2022-02-02 16:47:44 +0530

    add other ways for scrolling

--- script.js
+++ script.js
@@ -1,26 +1,44 @@
-var navBarAnchors = document.querySelectorAll('nav a');
+var navBarAnchors = document.querySelectorAll('nav a')
+var scrollInterval;
+for (var i = 0; i < navBarAnchors.length; i++) {
+  navBarAnchors[i].addEventListener('click', function (event) {
+    event.preventDefault()
+    for (let i in this.href) {
+      if (this.href[i] === '#') {
+        i = 1 * i
+        var targetSectionId = this.href.slice(i + 1)
+        break
+      }
+    }
+    var targetSection = document.getElementById(targetSectionId)
+    targetSectionCordinates = targetSection.getBoundingClientRect()
 
-for(var i = 0;i<navBarAnchors.length;i++){
-    navBarAnchors[i].addEventListener('click',function(event){
-        event.preventDefault()
-        for (let i in this.href) {
-            if(this.href[i] === '#'){
-                i = 1*i;
-                var targetSectionId = this.href.slice(i+1)
-                break;
-            }
-        }
-        var targetSection = document.getElementById(targetSectionId)
-        targetSectionCordinates = targetSection.getBoundingClientRect()
+    /** The One Way of doing */
+    //     var scrollInterval = setInterval(function () {
+    //       targetSectionCordinates = targetSection.getBoundingClientRect()
+    //       if (targetSectionCordinates.top <= 0) {
+    //         clearInterval(scrollInterval)
+    //         return
+    //       }
+    //       window.scrollBy(0, 100)
+    //     }, 15)
+    //   })
 
-        var scrollInterval = setInterval(function(event){
-            targetSectionCordinates = targetSection.getBoundingClientRect()
-            if(targetSectionCordinates.top <= 0){
-                clearInterval(scrollInterval);
-                return
-            }
-            window.scrollBy(0,100)
-        },15)
+    /** if function need agruments then pass it in pass it in third argument */
+    // scrollInterval = setInterval(scrollVertically, 15, targetSection)
 
-    })
+    /** Third way of doing the same thing */
+    scrollInterval = setInterval(function(){
+        scrollVertically(targetSection)
+    }, 15)
+  })
+}
+
+function scrollVertically(targetSection) {
+  targetSectionCordinates = targetSection.getBoundingClientRect()
+  if (targetSectionCordinates.top <= 0) {
+    clearInterval(scrollInterval)
+    return
+  }
+  window.scrollBy(0, 100)
 }
